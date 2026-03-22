9012155d1e30324708ed46090b30659be574f321
anil0015 <anil0015@algonquinlive.com>
2026-03-22 16:14:31 -0400

    clean repo (no terraform cache)

---
+++ infrastructure/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = ">= 1.2.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "a09-demo-rg"
+  location = "Canada Central"
+}
